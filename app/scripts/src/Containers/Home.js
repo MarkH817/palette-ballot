e2d6af927f1ee6487ae42dd41b50652acd7d2572
Mark Hernandez <markedcode@gmail.com>
2017-07-12 21:47:09 -0500

    Fix storage util. Move default data next to storage

--- app/scripts/src/Containers/Home.js
+++ app/scripts/src/Containers/Home.js
@@ -1,13 +1,34 @@
 import React, {Component} from 'react'
-import data from '../data.json'
 import Page from './Page'
 import Palette from '../Components/Palette'
+import {Storage} from '../utils'
 
 export default class Home extends Component {
-  render () {
-    let palettes = data.palettes.map(palette => {
+  constructor (props) {
+    super(props)
+    this.state = {
+      palettes: []
+    }
+
+    this.loadPalettes()
+  }
+
+  loadPalettes () {
+    Storage.getPalettes((savedPalettes) => {
+      this.setState({
+        palettes: savedPalettes
+      })
+    })
+  }
+
+  mapPalettes () {
+    return this.state.palettes.map((palette) => {
       return <Palette key={palette.id} title={palette.title} colors={palette.colors} />
     })
+  }
+
+  render () {
+    let palettes = this.mapPalettes()
 
     return (
       <Page section='home' title='Home'>
--- app/scripts/src/utils/storage.js
+++ app/scripts/src/utils/storage.js
@@ -1,31 +1,58 @@
-export function getSettings () {
+import defaults from './defaults.json'
+
+export function reset () {
   if (chrome.storage.sync) {
-    return chrome.storage.sync.get('settings')
+    chrome.storage.sync.clear()
+    chrome.storage.sync.set(defaults)
   }
 
-  return {}
+  console.log('Extension has been reset')
+}
+
+export function getSettings (cb) {
+  if (chrome.storage.sync) {
+    chrome.storage.sync.get('settings', (data) => {
+      if (data.settings !== undefined) {
+        cb(data.settings)
+      } else {
+        cb(defaults.settings)
+      }
+    })
+  } else {
+    cb(defaults.settings)
+  }
 }
 
 export function setSettings (settings, cb) {
   if (chrome.storage.sync) {
     chrome.storage.sync.set(settings, cb)
+  } else {
+    console.error(new Error('Browser not identified'))
   }
 }
 
-export function getPalettes () {
+export function getPalettes (cb) {
   if (chrome.storage.sync) {
-    return chrome.storage.sync.get('palettes')
+    chrome.storage.sync.get('palettes', (data) => {
+      if (data.palettes !== undefined) {
+        cb(data.palettes)
+      } else {
+        cb(defaults.palettes)
+      }
+    })
+  } else {
+    cb(defaults.palettes)
   }
-
-  return []
 }
 
 export function addPalette (palette, cb) {
   if (chrome.storage.sync) {
-    let data = {
-      palettes: getPalettes()
-    }
-    data.palettes.push(palette)
-    chrome.storage.sync.set(data, cb)
+    getPalettes((oldPalette) => {
+      chrome.storage.sync.set({
+        palette: oldPalette.push(palette)
+      }, cb)
+    })
+  } else {
+    console.error(new Error('Browser not identified'))
   }
 }
