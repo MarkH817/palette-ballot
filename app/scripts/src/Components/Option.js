1f53c289a54da983d7d8e98c228296046f02448d
Mark Hernandez <markedcode@gmail.com>
2017-08-03 14:43:45 -0500

    Describe prop inputs in comments

--- app/scripts/src/Components/Option.js
+++ app/scripts/src/Components/Option.js
@@ -4,29 +4,43 @@ export default class Option extends Component {
   render () {
     let Input
 
+    /*
+    props:
+    - type (string)
+    - label (string)
+    - name (string)
+    - value (string, boolean)
+    - values (string)
+    - handleChange (function)
+    */
+
     switch (this.props.type) {
       case 'dropdown':
         Input = (
-          <select name={this.props.name} value={this.props.value}>
-            {this.props.values.map((val, index) => (
-              <option key={index} value={val}>{val}</option>
-            ))}
-          </select>
+          <label>
+            {this.props.label}
+
+            <select name={this.props.name} value={this.props.value}>
+              {this.props.values.map((val, index) => (
+                <option key={index} value={val}>{val}</option>
+              ))}
+            </select>
+          </label>
         )
         break
       case 'checkbox':
-        Input = <input name={this.props.name} checked={this.props.value} type='checkbox' />
+        Input = (
+          <label>
+            <input name={this.props.name} checked={this.props.value} type='checkbox' />
+            {this.props.label}
+          </label>
+        )
         break
-      default:
-        Input = null
     }
 
     return (
       <section className='option'>
-        <label>
-          {this.props.label}
-          <Input />
-        </label>
+        {Input}
       </section>
     )
   }
